b907e050a347163e4c832d28e9adadc1f01c9fea
rafapradana <rafa.s.pradana@gmail.com>
2025-03-15 00:50:28 +0700

    font update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type { Config } from "tailwindcss";
 
 export default {
@@ -20,7 +19,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ["SF Pro Display", "Inter", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
